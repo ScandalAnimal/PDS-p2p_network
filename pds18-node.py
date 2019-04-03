04a3256a46def562e2bcffbfe2593cb3828cec2f
ScandalAnimal <mvasilisin@gmail.com>
2019-04-03 14:02:33 +0200

    socket connection peer/node + parsing arguments

--- pds18-node.py
+++ pds18-node.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python
 
+import socket
 import sys
+from parsers import parseNodeArgs
 
 print 'NODE'
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+
+args = parseNodeArgs()
+print 'ARGS: ', str(args)
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = ('localhost', 10000)
+print >> sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+
+while True:
+    print >> sys.stderr, '\nwaiting to receive message'
+    data, address = sock.recvfrom(4096)
+    
+    print >> sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    print >> sys.stderr, data
+    
+    if data:
+        sent = sock.sendto(data, address)
+        print >> sys.stderr, 'sent %s bytes back to %s' % (sent, address)
--- pds18-peer.py
+++ pds18-peer.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python
 
+import socket
 import sys
+from parsers import parsePeerArgs
 
 print 'PEER'
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+
+args = parsePeerArgs()
+print 'ARGS: ', str(args)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = ('localhost', 10000)
+message = 'This is the message.  It will be repeated.'
+
+try:
+
+    # Send data
+    print >> sys.stderr, 'sending "%s"' % message
+    sent = sock.sendto(message, server_address)
+
+    # Receive response
+    print >> sys.stderr, 'waiting to receive'
+    data, server = sock.recvfrom(4096)
+    print >> sys.stderr, 'received "%s"' % data
+
+finally:
+    print >> sys.stderr, 'closing socket'
+    sock.close()

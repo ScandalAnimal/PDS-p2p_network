d0ff6c2dd97584ae12e98cd733242e113ae3a524
ScandalAnimal <mvasilisin@gmail.com>
2019-04-05 18:04:37 +0200

    bencoder and sending HELLO from peer to node

--- pds18-peer.py
+++ pds18-peer.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 
 import socket
+import signal
 import sys
+import threading
+import time
 from parsers import parsePeerArgs
+from protocol import getHELLOMessage
+# , getGETLISTMessage, getLISTMessage, getMESSAGEMessage, getUPDATEMessage, getDISCONNECTMessage, getACKMessage, getERRORMessage
 
-print ("PEER")
+helloEvent = threading.Event()
 
-args = parsePeerArgs()
+def sendHello(sock, server_address, message):
+	while not helloEvent.is_set():
+		print ("sending %s" % message)
+		sent = sock.sendto(message.encode("utf-8"), server_address)
+		helloEvent.wait(10)
+
+	# TODO ... Clean shutdown code here ...
+	print ("CLEAN")
+
+class ServiceException(Exception):
+	pass
+
+def signalHandler(signum, frame):
+	print ('Caught signal %d' % signum) # TODO print to stderr
+	raise ServiceException
 
 class Peer:
 	def __init__(self, args):
@@ -21,26 +40,57 @@ class Peer:
 			", chatIp: " + self.chatIp + ", chatPort: " + str(self.chatPort) + 
 			", regIp: " + self.regIp + ", regPort: " + str(self.regPort))	
 
+
+print ("PEER")
+
+args = parsePeerArgs()
+
 peer = Peer(args)
 print ("Peer:" + str(peer))
 
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 server_address = (peer.regIp, peer.regPort)
-message = "This is the message.  It will be repeated."
+
+signal.signal(signal.SIGINT, signalHandler)
 
 try:
 
-    # Send data
-    print ("sending "%s"" % message)
-    sent = sock.sendto(message.encode("utf-8"), server_address)
+	helloMessage = getHELLOMessage(123, peer.username, peer.chatIp, peer.chatPort)
+	print ("hello: " + helloMessage)
+	helloThread = threading.Thread(target=sendHello, args=(sock, server_address), kwargs={"message": helloMessage})
+	helloThread.start()
 
-    # Receive response
-    print ("waiting to receive")
-    data, server = sock.recvfrom(4096)
-    print ("received "%s"" % data.decode("utf-8"))
+	# TODO add more functionality (recv?)
+	while True:
+		time.sleep(0.5)
+
+
+except ServiceException:
+
+	helloEvent.set()
+	helloThread.join()
+
+	# nulove HELLO pri kille
+	message = getHELLOMessage(123, peer.username, "0.0.0.0", 0)
+	print ("hello: " + message)
+	sent = sock.sendto(message.encode("utf-8"), server_address)
 
 finally:
-    print ("closing socket")
-    sock.close()
+	print ("closing socket")
+	sock.close()	
+
+# getGETLISTMessage(123)
+# getLISTMessage(123, "peers")
+# getMESSAGEMessage(123, 11, 11, "asd")
+# getUPDATEMessage(123, "db")
+# getDISCONNECTMessage(123)
+# getACKMessage(123)
+# getERRORMessage(123, "asasfad")
+
+
+
+#     # Receive response
+#     print ("waiting to receive")
+#     data, server = sock.recvfrom(4096)
+#     print ("received "%s"" % data.decode("utf-8"))
+
--- protocol.py
+++ protocol.py
@@ -1,28 +1,95 @@
 #!/usr/bin/python
 
+from bencoder import encode
+
+class Parent:
+	def toJson(self):
+		params = vars(self)
+		return {k.encode("utf-8"):v for k,v in params.items()}
+
+class Hello(Parent):
+	def __init__(self, txid, username, ipv4, port):
+		self.type = "hello"
+		self.txid = txid
+		self.username = username
+		self.ipv4 = ipv4
+		self.port = port
+
+class GetList(Parent):
+	def __init__(self, txid):
+		self.type = "getlist"
+		self.txid = txid
+
+class List(Parent):
+	def __init__(self, txid, peers):
+		self.type = "list"
+		self.txid = txid
+		self.peers = peers
+
+class Message(Parent):
+	def __init__(self, txid, fromIp, to, message):
+		self.type = "message"
+		self.txid = txid
+		self.fromIp = fromIp
+		self.to = to
+		self.message = message
+	def toJson(self):
+		params = vars(self)
+		return {k.replace("fromIp","from").encode("utf-8"):v for k,v in params.items()}	
+
+class Update(Parent):
+	def __init__(self, txid, db):
+		self.type = "update"
+		self.txid = txid
+		self.db = db
+
+class Disconnect(Parent):
+	def __init__(self, txid):
+		self.type = "disconnect"
+		self.txid = txid
+
+class Ack(Parent):
+	def __init__(self, txid):
+		self.type = "ack"
+		self.txid = txid
+
+class Error(Parent):
+	def __init__(self, txid, verbose):
+		self.type = "error"
+		self.txid = txid
+		self.verbose = verbose
+
 def getHELLOMessage(txid, username, ipv4, port):
-	messageType = "hello"
+	message = Hello(txid, username, ipv4, port)
+	return (encode(message.toJson()).decode())
 
 def getGETLISTMessage(txid):
-	messageType = "getlist"
+	message = GetList(txid)
+	print (encode(message.toJson()).decode())
 
 def getLISTMessage(txid, peers):
-	messageType = "list"
+	message = List(txid, peers)
+	print (encode(message.toJson()).decode())
 
-def getMESSAGEMessage(txid, from, to, message):
-	messageType = "message"
+def getMESSAGEMessage(txid, fromIp, to, message):
+	message = Message(txid, fromIp, to, message)
+	print (encode(message.toJson()).decode())
 
 def getUPDATEMessage(txid, db):
-	messageType = "update"
+	message = Update(txid, db)
+	print (encode(message.toJson()).decode())
 
 def getDISCONNECTMessage(txid):
-	messageType = "disconnect"
+	message = Disconnect(txid)
+	print (encode(message.toJson()).decode())
 
 def getACKMessage(txid):
-	messageType = "ack"
+	message = Ack(txid)
+	print (encode(message.toJson()).decode())
 
 def getERRORMessage(txid, verbose):
-	messageType = "error"
+	message = Error(txid, verbose)
+	print (encode(message.toJson()).decode())
 
 
 # PEER_RECORD := {"<ushort>":{"username":"<string>", "ipv4":"<dotted_decimal_IP>", "port": <ushort>}}
